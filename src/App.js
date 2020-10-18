c22c2db5d87d0a1ef983a2637ec27c8b9ae36d2e
Keith Billings <heyitsbo91@gmail.com>
2020-10-17 23:16:26 -0700

    refactor and add new project

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import 'aos/dist/aos.css';
 
 // Bootstrap components
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Row, Col, Navbar, Nav, Jumbotron, Card, Button, Accordion, OverlayTrigger, Tooltip } from "react-bootstrap";
+import { Container, Row, Col, Navbar, Nav, Jumbotron} from "react-bootstrap";
 
 // local 
 import './App.css';
@@ -20,7 +20,9 @@ import employeeDirectoryImage from "./images/thumbs/employeeDirectory.jpg";
 import partyPlannerImage from "./images/thumbs/PartyPlanner.png";
 import aboutYourBirthdayImage from "./images/thumbs/BirthdayApp.png";
 import weatherAppImage from "./images/thumbs/WeatherApp.png";
-import keithResume from "./images/Resume.pdf"
+import keithResume from "./images/Resume.pdf";
+import CodeProject from "./components/CodeProject";
+import advocateImage from "./images/advotecate.png"
 
 // Initializing the Animate on Scroll functionality
 AOS.init();
@@ -55,11 +57,11 @@ function App() {
             <Navbar.Brand>KB</Navbar.Brand>
             <Navbar.Toggle />
             <Navbar.Collapse className="justify-content-end">
-            <Nav className="mr-auto">
-              <Nav.Link href="#about">About Me</Nav.Link>
-              <Nav.Link href="#codingPortfolio">Coding Portfolio</Nav.Link>
-              <Nav.Link href="#contact">Contact</Nav.Link>
-            </Nav>
+              <Nav className="mr-auto">
+                <Nav.Link href="#about">About Me</Nav.Link>
+                <Nav.Link href="#codingPortfolio">Coding Portfolio</Nav.Link>
+                <Nav.Link href="#contact">Contact</Nav.Link>
+              </Nav>
             </Navbar.Collapse>
           </Navbar>
         </Col>
@@ -77,7 +79,7 @@ function App() {
         </Col>
       </Row>
       <Row className="padding">
-        <Col id="about">
+        <Col id="about" data-aos="fade-up">
           <h2>
             About Me
           </h2><br></br>
@@ -91,139 +93,25 @@ function App() {
       </Row>
       <Row><Col id="codingPortfolio"><br></br></Col></Row>
       <Row className="padding">
-        <Col>
+        <Col data-aos="fade-up">
           <h2>
             Coding Portfolio
           </h2>
         </Col>
       </Row>
-      <Row className="padding">
-        <Col data-aos="fade-up">
-          <Accordion>
-            <Card>
-              <Card.Img variant="top" src={`${employeeDirectoryImage}`} />
-              <Card.Body>
-                <Accordion.Toggle className="onHoverTextToBlue onHoverPointer" as={Card.Title} eventKey="0">
-                  {<OverlayTrigger
-                    key={"bottom"}
-                    placement={"bottom"}
-                    overlay={
-                      <Tooltip id={`tooltip1`}>
-                        Click to learn more
-                    </Tooltip>
-                    }>
-                    <Card.Title>
-                      Employee Directory
-                  </Card.Title>
-                  </OverlayTrigger>
-                  }
-                </Accordion.Toggle>
-                <Accordion.Collapse eventKey="0">
-                  <Card.Body>
-                    This is a React app that maps over data and creates a stylized, sortable, and filterable table.
-                  </Card.Body>
-                </Accordion.Collapse>
-                <Button href="https://employee-directory-kmb.herokuapp.com/" variant="link">Deployment</Button>
-                <Button href="https://github.com/KeithBillings/Employee-Directory" variant="link">GitHub Repository</Button>
-              </Card.Body>
-            </Card>
-          </Accordion>
-        </Col>
-        <Col data-aos="fade-up">
-          <Accordion>
-            <Card>
-              <Card.Img variant="top" src={`${partyPlannerImage}`} />
-              <Card.Body>
-                <Accordion.Toggle className="onHoverTextToBlue onHoverPointer" as={Card.Title} eventKey="0">
-                  {<OverlayTrigger
-                    key={"bottom"}
-                    placement={"bottom"}
-                    overlay={
-                      <Tooltip id={`tooltip1`}>
-                        Click to learn more
-                    </Tooltip>
-                    }>
-                    <Card.Title>
-                      Party Planning Committee
-                  </Card.Title>
-                  </OverlayTrigger>
-                  }
-                </Accordion.Toggle>
-                <Accordion.Collapse eventKey="0">
-                  <Card.Body>This is my second major group project. It was a joint effort between myself, Max Edmonds, Brian Soldani, and Alex Shafer. This project was our first fully full-stack project. I learned a lot about saving information on a database, using the db to serve different pages, how to create working routers, and using homemade APIs. I learned about backend the most on this project. I also noticed by observing my classmates that my front end CSS and styling skills are also something I have room to grow on. My backend and logic skills are really coming along, and I am proud of that, but to be a proper full stack developer you need to be well rounded in all areas!</Card.Body>
-                </Accordion.Collapse>
-                <Button href="https://partyplanningcommittee.herokuapp.com/" variant="link">Deployment</Button>
-                <Button href="https://github.com/KeithBillings/Party-Planner" variant="link">GitHub Repository</Button>
-              </Card.Body>
-            </Card>
-          </Accordion>
-        </Col>
+      <Row className="padding" >
+        <CodeProject projectTitle={"adVOTEcate"} projectDescription={"This was our final group project of our bootcamp. This app is your advocate to your voting needs! We aim to provide users easily accesible information to check if they are registered to vote, learn how to register if they are not, search where the nearest ballot drop boxes are, and set reminders!"} deploymentLink={"https://advotecate.herokuapp.com/"} repositoryLink={"https://github.com/KeithBillings/adVOTEcate"} image={advocateImage} />
       </Row>
       <Row className="padding">
-        <Col data-aos="fade-up">
-          <Accordion>
-            <Card>
-              <Card.Img variant="top" src={`${aboutYourBirthdayImage}`} />
-              <Card.Body>
-                <Accordion.Toggle className="onHoverTextToBlue onHoverPointer" as={Card.Title} eventKey="0">
-                  {<OverlayTrigger
-                    key={"bottom"}
-                    placement={"bottom"}
-                    overlay={
-                      <Tooltip id={`tooltip1`}>
-                        Click to learn more
-                    </Tooltip>
-                    }>
-                    <Card.Title>
-                      About Your Birthday App
-                  </Card.Title>
-                  </OverlayTrigger>
-                  }
-                </Accordion.Toggle>
-                <Accordion.Collapse eventKey="0">
-                  <Card.Body>
-                    This was our first major class project involving teams. The class was divided into groups of 3-4 members and we were allowed to research and come up with our own project to work on. Since this was our first team assignment, and our first self directed project, our team quickly decided that the project must be something fun and entertaining. It didn't take much time to focus on a topic that everyone could relate to: Birthdays! We learned how to use external APIs like NASA, birthday facts, number facts, zoidac, and more!
-                  </Card.Body>
-                </Accordion.Collapse>
-                <Button href="https://keithbillings.github.io/About-Your-Birthday/" variant="link">Deployment</Button>
-                <Button href="https://github.com/KeithBillings/About-Your-Birthday" variant="link">GitHub Repository</Button>
-              </Card.Body>
-            </Card>
-          </Accordion>
-        </Col>
-        <Col data-aos="fade-up">
-          <Accordion>
-            <Card>
-              <Card.Img variant="top" src={`${weatherAppImage}`} />
-              <Card.Body>
-                <Accordion.Toggle className="onHoverTextToBlue onHoverPointer" as={Card.Title} eventKey="0">
-                  {<OverlayTrigger
-                    key={"bottom"}
-                    placement={"bottom"}
-                    overlay={
-                      <Tooltip id={`tooltip1`}>
-                        Click to learn more
-                    </Tooltip>
-                    }>
-                    <Card.Title>
-                      Weather App
-                  </Card.Title>
-                  </OverlayTrigger>
-                  }
-                </Accordion.Toggle>
-                <Accordion.Collapse eventKey="0">
-                  <Card.Body>
-                    On this homework assignment I was tasked with creating a weather dashboard. It is stylish and minimal. It has a rotating background of seasonal images, and a sleek design. Enter the city query in the text input box and it will return current live weather data, and a 5 day forecast. The site also logs your search history. When you first visit the site, you can click on one of your past searches and it will search that city again. This uses AJAX calls to a weather API to GET current weather information.                  </Card.Body>
-                </Accordion.Collapse>
-                <Button href="https://keithbillings.github.io/Weather-Dashboard/" variant="link">Deployment</Button>
-                <Button href="https://github.com/KeithBillings/Weather-Dashboard" variant="link">GitHub Repository</Button>
-              </Card.Body>
-            </Card>
-          </Accordion>
-        </Col>
+        <CodeProject projectTitle={"Employee Directory"} projectDescription={"This is a React app that maps over data and creates a stylized, sortable, and filterable table."} deploymentLink={"https://employee-directory-kmb.herokuapp.com/"} repositoryLink={"https://github.com/KeithBillings/Employee-Directory"} image={employeeDirectoryImage} />
+        <CodeProject data-aos="fade-up" projectTitle={"Party Planning Committee"} projectDescription={"This is my second major group project. It was a joint effort between myself, Max Edmonds, Brian Soldani, and Alex Shafer. This project was our first fully full-stack project. I learned a lot about saving information on a database, using the db to serve different pages, how to create working routers, and using homemade APIs. I learned about backend the most on this project. I also noticed by observing my classmates that my front end CSS and styling skills are also something I have room to grow on. My backend and logic skills are really coming along, and I am proud of that, but to be a proper full stack developer you need to be well rounded in all areas!"} deploymentLink={"https://partyplanningcommittee.herokuapp.com/"} repositoryLink={"https://github.com/KeithBillings/Party-Planner"} image={partyPlannerImage} />
       </Row>
       <Row className="padding">
-        <Col id="contact">
+        <CodeProject projectTitle={"About Your Birthday App"} projectDescription={"This was our first major class project involving teams. The class was divided into groups of 3-4 members and we were allowed to research and come up with our own project to work on. Since this was our first team assignment, and our first self directed project, our team quickly decided that the project must be something fun and entertaining. It didn't take much time to focus on a topic that everyone could relate to: Birthdays! We learned how to use external APIs like NASA, birthday facts, number facts, zoidac, and more!"} deploymentLink={"https://keithbillings.github.io/About-Your-Birthday/"} repositoryLink={"https://github.com/KeithBillings/About-Your-Birthday"} image={aboutYourBirthdayImage} />
+        <CodeProject projectTitle={"Weather App"} projectDescription={"On this homework assignment I was tasked with creating a weather dashboard. It is stylish and minimal. It has a rotating background of seasonal images, and a sleek design. Enter the city query in the text input box and it will return current live weather data, and a 5 day forecast. The site also logs your search history. When you first visit the site, you can click on one of your past searches and it will search that city again. This uses AJAX calls to a weather API to GET current weather information."} deploymentLink={"https://keithbillings.github.io/Weather-Dashboard/"} repositoryLink={"https://github.com/KeithBillings/Weather-Dashboard"} image={weatherAppImage} />
+      </Row>
+      <Row className="padding">
+        <Col id="contact" data-aos="fade-up">
           <h2>
             Contact Me
           </h2>
---
+++ src/components/CodeProject/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Col, Card, Button, Accordion, OverlayTrigger, Tooltip } from "react-bootstrap";
+
+function CodeProject({projectTitle, projectDescription, deploymentLink, image, repositoryLink}) {
+
+  return (
+    <Col data-aos="fade-up">
+      <Accordion>
+        <Card>
+          <Card.Img variant="top" src={image} />
+          <Card.Body>
+            <Accordion.Toggle className="onHoverTextToBlue onHoverPointer" as={Card.Title} eventKey="0">
+              {<OverlayTrigger
+                key={"bottom"}
+                placement={"bottom"}
+                overlay={
+                  <Tooltip id={`tooltip1`}>
+                    Click to learn more
+                    </Tooltip>
+                }>
+                <Card.Title>
+                  {projectTitle}
+                  </Card.Title>
+              </OverlayTrigger>
+              }
+            </Accordion.Toggle>
+            <Accordion.Collapse eventKey="0">
+              <Card.Body>
+                {projectDescription}
+                  </Card.Body>
+            </Accordion.Collapse>
+            <Button href={deploymentLink} variant="link">Deployment</Button>
+            <Button href={repositoryLink} variant="link">GitHub Repository</Button>
+          </Card.Body>
+        </Card>
+      </Accordion>
+    </Col>
+  )
+}
+
+export default CodeProject
