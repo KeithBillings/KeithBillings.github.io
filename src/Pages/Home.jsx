e42913895f93f2834d317162695ca54a58c81d73
Keith Billings <keith.billings@brandlive.com>
2023-04-23 19:58:16 -0700

    hero styling and animation

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,19 +1,55 @@
+import { useRef, useEffect } from "react";
+
+// Image
 import keith from "../assets/images/IMG_0944.png";
 
 export default function Home() {
-  // use the useContext hook to access the WindowSize context
+  // Refs
+  const greeting = useRef(null);
+  const title = useRef(null);
 
-  // Title card for the home page. Has text on the left and an image on the right. The text will have a greeting, my name, and my title. The image will be a picture of me.
+  useEffect(() => {
+    // Greeting animation
+    const greetingText = "Hello, World! I'm,";
+    // Clear greeting
+    greeting.current.innerHTML = "";
+    // Loop through each letter in greetingText
+    for (let i = 0; i < greetingText.length; i++) {
+      setTimeout(() => {
+        greeting.current.innerHTML += greetingText[i];
+      }, 80 * i);
+      // 80ms delay between each letter
+      // total delay = 80 * greetingText.length = 80 * 18 = 1440ms
+    }
+
+    // Title animation
+    const titleText = "Web developer";
+    // Clear title
+    title.current.innerHTML = "";
+    // Loop through each letter in titleText
+    for (let i = 0; i < titleText.length; i++) {
+      setTimeout(() => {
+        title.current.innerHTML += titleText[i];
+      }, 80 * i + 1440);
+    }
+  }, []);
+
   function Hero() {
     return (
-      <div className={"hero"}>
-        <div className="hero-text">
-          <h1 className="hero-text__greeting">Hello, World! I'm,</h1>
-          <h1 className="hero-text__name">Keith Billings</h1>
-          <h1 className="hero-text__title">Web developer</h1>
-        </div>
-        <div className="hero-image">
-          <img src={keith} alt="Keith Billings" />
+      <div className="hero">
+        <div className="hero-container">
+          <div className="hero-text">
+            <h1 className="hero-text__greeting" ref={greeting}>
+              Hello, World! I'm,
+            </h1>
+            <h1 className="hero-text__name">Keith Billings</h1>
+            <h1 className="hero-text__title" ref={title}>
+              Web developer
+            </h1>
+          </div>
+          <div className="hero-image">
+            <img src={keith} alt="Keith Billings" />
+          </div>
         </div>
       </div>
     );
