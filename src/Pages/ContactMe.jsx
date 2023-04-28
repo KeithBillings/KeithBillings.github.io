8fb8a1286edb87c4b03004e4318eaee26ecf1bac
Keith Billings <keith.billings@brandlive.com>
2023-04-28 00:40:00 -0700

    add emailing logic

--- src/Pages/ContactMe.jsx
+++ src/Pages/ContactMe.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+
+// Import React Router
 import { Link } from "react-router-dom";
 
+// Import Axios
+import axios from "axios";
+
 export default function ContactMe() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post("https://keithbillings-api.herokuapp.com/send-email", {
+        name,
+        email,
+        message,
+      });
+
+      if (response.status === 200) {
+        alert("Email sent successfully!");
+      }
+    } catch (error) {
+      console.error("Error sending email:", error);
+      alert("Error sending email");
+    }
+  };
+
   return (
     <div className="contact-me">
       <h1 className="contact-me__title">Contact Me</h1>
@@ -24,18 +52,18 @@ export default function ContactMe() {
         </li>
       </ul>
       {/* Contact Me form with Nodemailer */}
-      <form className="contact-me__form">
+      <form className="contact-me__form" onSubmit={handleSubmit}>
         <div className="contact-me__form__group">
           <label htmlFor="name">Name</label>
-          <input type="text" name="name" id="name" placeholder="First Last" />
+          <input type="text" name="name" id="name" placeholder="First Last" value={name} onChange={(e) => setName(e.target.value)} />
         </div>
         <div className="contact-me__form__group">
           <label htmlFor="email">Email</label>
-          <input type="email" name="email" id="email" placeholder="example@example.com" />
+          <input type="email" name="email" id="email" placeholder="example@example.com" value={email} onChange={(e) => setEmail(e.target.value)} />
         </div>
         <div className="contact-me__form__group">
           <label htmlFor="message">Message</label>
-          <textarea name="message" id="message" cols="30" rows="10"></textarea>
+          <textarea name="message" id="message" cols="30" rows="10" value={message} onChange={(e) => setMessage(e.target.value)}></textarea>
         </div>
         <div className="contact-me__form__buttons">
           <button type="submit">Send</button>
