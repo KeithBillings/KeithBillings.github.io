6d1fb0d97664fe73f20e6ffb01f0082c4f3571a4
Keith Billings <keith.billings@brandlive.com>
2023-05-03 16:22:49 -0700

    add privacy policy

--- src/Pages/Pages.jsx
+++ src/Pages/Pages.jsx
@@ -5,12 +5,14 @@ import { Routes, Route } from "react-router-dom";
 import NotFound from "./NotFound";
 import Home from "./Home";
 import ContactMe from "./ContactMe";
+import PrivacyPolicy from "./PrivacyPolicy";
 
 export default function Pages() {
   return (
     <Routes>
       <Route exact path="/about" element={<Home scrollTo={"about"} />} />
       <Route exact path="/contact" element={<ContactMe />} />
+      <Route exact path="/privacy-policy" element={<PrivacyPolicy />} />
       <Route exact path="/" element={<Home />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom";
+
 export default function Footer() {
   return (
     <footer className="footer">
       <div className="footer__content">
+        <Link to="/privacy-policy" className="footer__privacy-policy">Privacy Policy</Link>
         <p>&copy; {new Date().getFullYear()} Keith Billings. All rights reserved.</p>
       </div>
     </footer>
