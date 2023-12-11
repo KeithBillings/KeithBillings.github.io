091343ebeaa6caa6008764db3419dd54be2656ed
Keith Billings <keithbillingsbusiness@gmail.com>
2023-12-10 22:25:12 -0800

    css changes to main modules on the home page, fixing the grayscale on the companies for safari

--- src/components/Companies/Companies.jsx
+++ src/components/Companies/Companies.jsx
@@ -1,14 +1,20 @@
-import { companies } from "../../assets/data/companies";
+import { companies } from '../../assets/data/companies';
 
 export default function Companies() {
   return (
-    <section className="companies">
-      <h2 className="companies__title">Companies I've worked with</h2>
-      <div className="companies__container">
+    <section className='companies'>
+      <h2 className='companies__title'>Companies I've worked with</h2>
+      <div className='companies__container'>
         {companies.map((company, index) => (
-          <div key={index} className="companies__item">
-            <svg className="companies__item__logo" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" style={{ WebkitMaskImage: `url(${company.logo})`, maskImage: `url(${company.logo})` }}>
-              <image className="companies__item__logo__image " href={company.logo} height="100" width="100" />
+          <div key={index} className='companies__item'>
+            <svg
+              className='companies__item__logo'
+              viewBox='0 0 100 100'
+              xmlns='http://www.w3.org/2000/svg'
+              style={{ WebkitMaskImage: `url(${company.logo})`, maskImage: `url(${company.logo})` }}
+            >
+              <image className='companies__item__logo__image ' href={company.logo} height='100' width='100' filter='url(#grayscale)' />
+              {/* Applying grayscale filter on the component directly due to Safari compatibility issues. The 'grayscale' filter is defined globally in the app's head as an svg def matrix. */}
             </svg>
           </div>
         ))}
