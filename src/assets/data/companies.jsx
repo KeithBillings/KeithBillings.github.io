7f24b8ba68ab884bb35bfdfafd75b46049572634
Keith Billings <keithbillingsbusiness@gmail.com>
2024-06-08 20:28:59 -0700

    update company logos

--- src/assets/data/companies.jsx
+++ src/assets/data/companies.jsx
@@ -1,18 +1,13 @@
 import aarpLogo from "../svgs/aarp-logo.svg";
 import adidasLogo from "../svgs/adidas-logo.svg";
-import arkInvestmentsLogo from "../svgs/ark-investments-logo.svg";
 import asanaLogo from "../svgs/asana-logo.svg";
-import bidenCampaignLogo from "../svgs/biden-campaign-logo.svg";
 import blockLogo from "../images/block-logo.png";
 import clarivateLogo from "../svgs/clarivate-logo.svg";
 import clickUpLogo from "../svgs/clickup-logo.svg";
 import columbiaLogo from "../svgs/columbia-logo.svg";
-import condeNastLogo from "../svgs/conde-nast-logo.svg";
-import deloitteLogo from "../svgs/deloitte-logo.svg";
 import dowJonesLogo from "../svgs/dow-jones-logo.svg";
 import googleLogo from "../svgs/google-logo.svg";
 import hyundaiLogo from "../svgs/hyundai-logo.svg";
-import nmaiLogo from "../images/national-museum-of-the-american-indian-logo.png";
 import nikeLogo from "../svgs/nike-logo.svg";
 import nintendoLogo from "../svgs/nintendo-logo.svg";
 import northFaceLogo from "../svgs/north-face-logo.svg";
@@ -20,10 +15,8 @@ import nprLogo from "../svgs/npr-logo.svg";
 import pinkLogo from "../images/vs-pink-logo.png";
 import pluralsightLogo from "../svgs/pluralsight-logo.svg";
 import publicLogo from "../svgs/public-logo.svg";
-import theBWordLogo from "../svgs/the-b-word-logo.svg";
-import usanaLogo from "../svgs/usana-logo.svg";
+import sonyLogo from "../svgs/sony-logo.svg";
 import vansLogo from "../svgs/vans-logo.svg";
-import wallStreetJournalLogo from "../svgs/wsj-logo.svg";
 import wiredLogo from "../svgs/wired-logo.svg";
 import youtubeLogo from "../svgs/youtube-logo.svg";
 
@@ -37,16 +30,8 @@ export const companies = [
     logo: adidasLogo,
   },
   {
-    name: "Ark Investments",
-    logo: arkInvestmentsLogo,
-  },
-  {
     name: "Asana",
     logo: asanaLogo,
-  },
-  {
-    name: "Biden Campaign",
-    logo: bidenCampaignLogo,
   },
   {
     name: "Block Inc.",
@@ -65,14 +50,6 @@ export const companies = [
     logo: columbiaLogo,
   },
   {
-    name: "Condé Nast",
-    logo: condeNastLogo,
-  },
-  {
-    name: "Deloitte",
-    logo: deloitteLogo,
-  },
-  {
     name: "Dow Jones",
     logo: dowJonesLogo,
   },
@@ -83,10 +60,6 @@ export const companies = [
   {
     name: "Hyundai",
     logo: hyundaiLogo,
-  },
-  {
-    name: "National Museum of the American Indian",
-    logo: nmaiLogo,
   },
   {
     name: "Nike",
@@ -117,20 +90,12 @@ export const companies = [
     logo: publicLogo,
   },
   {
-    name: "The B Word",
-    logo: theBWordLogo,
-  },
-  {
-    name: "Usana",
-    logo: usanaLogo,
+    name: "Sony",
+    logo: sonyLogo,
   },
   {
     name: "Vans",
     logo: vansLogo,
-  },
-  {
-    name: "Wall Street Journal",
-    logo: wallStreetJournalLogo,
   },
   {
     name: "Wired",
